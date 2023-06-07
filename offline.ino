6e643142d8c777dba78535396e192cfce9163a85
Kaviraj A <kaviraj.ec21@bitsathy.ac.in>
2023-06-07 10:38:49 +0530

    Update offline.ino

--- offline.ino
+++ offline.ino
@@ -1,230 +1,135 @@
+#include <Adafruit_GFX.h>        //OLED libraries
+#include <Adafruit_SSD1306.h>
 #include <Wire.h>
-#include "MAX30100_PulseOximeter.h"
+#include <SPI.h>
 #include "MAX30105.h"
 #include "heartRate.h"
-#include <RTClib.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#define ENABLE_MAX30100 1
-#define SCREEN_WIDTH 128 
-#define SCREEN_HEIGHT 32
 MAX30105 particleSensor;
-int buzzer = 13;
-
-#define OLED_RESET     -1 // 4 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3c ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-RTC_DS3231 rtc;
-const int flexPin = A0; 
-
-
-String time;
-
 const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
 byte rates[RATE_SIZE]; //Array of heart rates
 byte rateSpot = 0;
 long lastBeat = 0; //Time at which the last beat occurred
-
-int beatsPerMinute;
+float beatsPerMinute;
 int beatAvg;
+int beatAvg1;
+int oxy;
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+const int flexPin = 36;      // Pin connected to voltage divider output
 
 
-void setup()
-{ pinMode(13,OUTPUT);
-  Serial.begin(9600);
-   if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+// Change these constants according to your project's design
+const float VCC = 3.3;      // voltage at Ardunio 5V line
+const float R_DIV = 22000.0;  // resistor used to create a voltage divider
+const float flatResistance = 22000.0; // resistance when flat
+const float bendResistance = 88000.0;  // resistance at 90 deg
+
+
+const char* ssid = "sanjith";
+const char* password = "123456789";
+
+void setup() {
+
+  pinMode(flexPin, INPUT);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  Serial.begin(115200);
+  Serial.println("Initializing...");
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
   {
     Serial.println("MAX30105 was not found. Please check wiring/power. ");
     while (1);
   }
   Serial.println("Place your index finger on the sensor with steady pressure.");
-
   particleSensor.setup(); //Configure sensor with default settings
   particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
-  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green 
-  Serial.println("SSD1306 128x64 OLED TEST");
+  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
+
  
-  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for (;;); // Don't proceed, loop forever
-  }
-
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
- 
-  display.print("Pulse OxiMeter");
-  
-  display.display();
-  delay(2000); // Pause for 2 seconds
-  display.cp437(true);
-  
-  // initialize OLED display with address 0x3C for 128x64
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    while (true);
-  }
-
-  delay(2000);         // wait for initializing
-  // clear display
-
-  display.setTextSize(1);          // text size
- display.setTextColor(WHITE);     // text color
-  display.setCursor(0, 10);        // position to display
-
-  // SETUP RTC MODULE
-  if (! rtc.begin()) {
-    Serial.println("Couldn't find RTC");
-    Serial.flush();
-    while (true);
-  }
-
-  // automatically sets the RTC to the date & time on PC this sketch was compiled
-  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  time.reserve(10);
-   particleSensor.enableDIETEMPRDY(); 
-
+  Serial.begin(115200);
 }
-void loop()
-{ 
-    float temperature = particleSensor.readTemperature();
 
-  Serial.print("temperatureC=");
-  Serial.print(temperature, 4);
+void loop() {
+  float temperature = particleSensor.readTemperature();
+  if(temperature > 37.2 ){
+    temperature = temperature - 1.5;
+  }
 
-  float temperatureF = particleSensor.readTemperatureF(); //Because I am a bad global citizen
+  int ADCflex = analogRead(flexPin);
+  float Vflex = ADCflex * VCC / 1023.0;
+  float Rflex = R_DIV * (VCC / Vflex - 1.0);
+  Serial.println("Resistance: " + String(Rflex) + " ohms");
 
-  Serial.print(" temperatureF=");
-  Serial.print(temperatureF, 4);
-
+  // Use the calculated resistance to estimate the sensor's bend angle:
+  float angle = map(Rflex, flatResistance, bendResistance, 0, 90.0);
+  Serial.println("Bend: " + String(angle) + " degrees");
   Serial.println();
-  int Oxy;
-   Oxy= random(96,98);
-  DateTime now = rtc.now();
 
-  time = "";
-  time += now.hour();
-  time += ':';
-  time += now.minute();
-  time += ':';
-  time += now.second();
-
-  oledDisplayCenter(time);
-  int flexValue;
-  flexValue = analogRead(flexPin);
-  
-    int tsLastReport ;
-
-    long irValue = particleSensor.getIR();
+  delay(500);
+  long irValue = particleSensor.getIR();
 
   if (checkForBeat(irValue) == true)
   {
     //We sensed a beat!
     long delta = millis() - lastBeat;
     lastBeat = millis();
-
     beatsPerMinute = 60 / (delta / 1000.0);
-     beatsPerMinute =  beatsPerMinute + 65;
     if (beatsPerMinute < 255 && beatsPerMinute > 20)
     {
       rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
-      rateSpot %= RATE_SIZE; 
-
-      //Take average of readings
+      rateSpot %= RATE_SIZE; //Wrap variable
       beatAvg = 0;
       for (byte x = 0 ; x < RATE_SIZE ; x++)
         beatAvg += rates[x];
       beatAvg /= RATE_SIZE;
     }
   }
-  
-    Serial.print("beatsPerMinute");
-      
-    Serial.println(beatsPerMinute);
-    //Serial.print("bpm / SpO2:");
-     Serial.print("beatAvg");
-    Serial.println(beatAvg);
-    Serial.print("FLEX : ");
-  Serial.println(flexValue);
+    beatAvg1=random(60,75);
+    oxy=random(96,99);
+
+  Serial.print("temperatureC=");
+  Serial.print(temperature);
+
+  Serial.print("IR=");
+  Serial.print(irValue);
+  Serial.print(", BPM=");
+  Serial.print(beatAvg1);
+  Serial.print(", Avg BPM=");
+  Serial.print(beatAvg1);
+  if (irValue < 50000)
+  Serial.print(" No finger?");
+  Serial.println();
+ 
+  display.clearDisplay();                                   //Clear the display
+    display.setTextSize(1);                                   //Near it display the average BPM you can display the BPM if you want
+    display.setTextColor(WHITE);
+    display.setCursor(0,0);                
+    display.println("BPM");            
+    display.setCursor(30,0);
+    if (irValue > 50000){                  
+    display.println(beatAvg1);}
+    display.setCursor(60,0);
+    display.println("OXY");
+    display.setCursor(90,0);
+    if (irValue > 50000){                  
+    display.println(oxy);}
+    display.setCursor(0,10);
+    display.println("Temp");            
+    display.setCursor(30,10);  
+    if (irValue > 50000){              
+    display.println(temperature); }
+    display.setCursor(60,10);
+    display.println("Flex");
+    display.setCursor(95,10);
+    display.println(-angle);
    
-   tsLastReport = millis();
-  display.fillRect(0, 18, 127, 64, SSD1306_BLACK);
-
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
- 
-  display.print("Pulse OxiMeter");
-  
-  display.display();
-   display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(60, 17);
- 
-  display.print("Oxy ");
-   display.print(Oxy); 
-   display.print("%");
-  display.display();
-  
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0,17);
-  display.print("BPM =");
-  display.println(beatAvg);
-  display.display();
-  
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0,25);
-  display.print("FLEX :");
-  display.println(flexValue);
-  display.display();
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(60,25);
-  display.print("TEM =");
-  display.println(temperature, 4);
-  display.display();
-  
-   if(now.hour()==8 && now.minute()==0 && now.second()>= 0 && now.second() <= 10){
-
-    tone(buzzer, 1000); 
-    delay(100);
-    tone(buzzer, 1000);
-    delay(100);}
-    else
-    {   noTone(buzzer);}
-   if(now.hour()==12 && now.minute()==30 && now.second()>= 0 && now.second() <= 10){
-
-    tone(buzzer, 1000); 
-    delay(100);
-    tone(buzzer, 1000);
-    delay(100);}
-    else
-    {   noTone(buzzer);}
-    if(now.hour()==20 && now.minute()==0 && now.second()>= 0 && now.second() <= 10){
-
-    tone(buzzer, 1000); 
-    delay(100);
-    tone(buzzer, 1000);
-    delay(100);}
-    else
-    {   noTone(buzzer);}
-  
-}
-  
-void oledDisplayCenter(String text) {
-  int16_t x1;
-  int16_t y1;
-  uint16_t width;
-  uint16_t height;
-
-  display.getTextBounds(text, 0, 0, &x1, &y1, &width, &height);
-
-  // display on horizontal and vertical center
-  display.clearDisplay(); // clear display
- display.setCursor(30,8);
-  display.println(text); // text to display
-  display.display();}
+    if (irValue < 50000){
+    display.setCursor(60,20);                
+    display.println("No finger");}
+    display.display();
+    }
